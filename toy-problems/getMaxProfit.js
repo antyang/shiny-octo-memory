16ffbcb3fd82157c4bfd860e2a9bf08ecea3c6ec
antyang <mrant0ny@gmail.com>
2016-09-08 13:55:51 -0700

    Fix spacing in getMaxProfit

--- toy-problems/getMaxProfit.js
+++ toy-problems/getMaxProfit.js
@@ -1,20 +1,20 @@
 /**
-	* Suppose we could access yesterday's stock prices as an array, where:
+* Suppose we could access yesterday's stock prices as an array, where:
 
-	* The indices are the time in minutes past trade opening time, which was 9:30am
-	* local time.
-	*
-	* The values are the price in dollars of Apple stock at that time.
-	* So if the stock cost $500 at 10:30am, stockPricesYesterday[60] = 500.
+* The indices are the time in minutes past trade opening time, which was 9:30am
+* local time.
+*
+* The values are the price in dollars of Apple stock at that time.
+* So if the stock cost $500 at 10:30am, stockPricesYesterday[60] = 500.
 
-	* Write an efficient function that takes stockPricesYesterday and returns the best
-	* profit I could have made from 1 purchase and 1 sale of 1 Apple stock yesterday.
+* Write an efficient function that takes stockPricesYesterday and returns the best
+* profit I could have made from 1 purchase and 1 sale of 1 Apple stock yesterday.
 
-	* @param arr of int
-	* @return int
-	*
-	* Time: linear o(n)
-	* Space: constant o(1)
+* @param arr of int
+* @return int
+*
+* Time: linear o(n)
+* Space: constant o(1)
 */
 
 function getMaxProfit(arr) {
@@ -27,14 +27,14 @@ function getMaxProfit(arr) {
 	var maxProfit = arr[1] - arr[0];
 
 	for (var i = 1; i < arr.length; i++) {
-			var currentPrice = arr[i];
+		var currentPrice = arr[i];
 
-			var potentialProfit = currentPrice - minPrice;
+		var potentialProfit = currentPrice - minPrice;
 
-			maxProfit = Math.max(maxProfit, potentialProfit);
-			minPrice = Math.min(minPrice, currentPrice)
+		maxProfit = Math.max(maxProfit, potentialProfit);
+		minPrice = Math.min(minPrice, currentPrice)
 
-			// console.log(currentPrice, potentialProfit, maxProfit, minPrice);
+		// console.log(currentPrice, potentialProfit, maxProfit, minPrice);
 	}
 	return maxProfit;
 }
