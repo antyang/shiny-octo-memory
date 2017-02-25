dd3f0a6f7121376ed44fabd2ec133f582aa68a0e
antyang <mrant0ny@gmail.com>
2017-02-24 17:07:25 -0800

    Add Transpose Two Strings problem

---
+++ toy-problems/transposeTwoStrings.js
@@ -0,0 +1,25 @@
+function transposeTwoStrings(arrOfStrings) {
+  var str1 = arrOfStrings[0];
+  var str2 = arrOfStrings[1];
+  var newStr1 = str1.split('');
+  var newStr2 = str2.split('');
+  var result = [];
+
+  for(var i = 0; i < newStr1.length || i < newStr2.length; i++) {
+    if(newStr1[i] === undefined) {
+      result.push('  ' + newStr2[i]);
+    } else if (newStr2[i] === undefined) {
+      result.push(newStr1[i] + '  ');
+    } else {
+      result.push(newStr1[i] + ' ' + newStr2[i]);
+    }
+  }
+  return result.join('\n');
+}
+
+transposeTwoStrings(['Hello','World']);
+// H W  
+// e o  
+// l r  
+// l l  
+// o d
