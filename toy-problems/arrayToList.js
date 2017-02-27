43da15a5a7aa12d28c4b630c2c6c01ad7525689b
antyang <mrant0ny@gmail.com>
2017-02-26 22:02:02 -0800

    Add Array To List problem

---
+++ toy-problems/arrayToList.js
@@ -0,0 +1,11 @@
+var arrayToList = function(arr) {
+	while(arr.length) {
+		return {
+			value : arr[0],
+			rest : arr[1] === null ? null : arrayToList(arr.splice(1)),
+		}
+	}
+}
+
+console.log(arrayToList([10, 20]));
+// → {value: 10, rest: {value: 20, rest: null}}
