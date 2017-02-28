088ca2b0aba42aa3102ca7ce0a0411ffaa1da611
antyang <mrant0ny@gmail.com>
2017-02-28 12:18:47 -0800

    Modify insert method

--- data-structures/singlyLinkedList.js
+++ data-structures/singlyLinkedList.js
@@ -2,7 +2,7 @@ var LinkedList = function() {
 	this.head = null;
 }
 
-LinkedList.prototype.insert = function(val) {
+LinkedList.prototype.insertAfter = function(val) {
 	var node = {
         data: val,
         next:null,
@@ -22,14 +22,13 @@ LinkedList.prototype.insert = function(val) {
 
 var sll = new LinkedList();
 
-sll.insert(1)
-sll.insert(2)
-sll.insert(3)
-sll.insert(4)
-sll.insert(5)
-sll.insert(6)
-sll.insert(7)
-sll.insert(8)
-sll.insert(9)
-
-console.log(JSON.stringify(sll, null, 2))
+sll.insertAfter(1);
+sll.insertAfter(2);
+sll.insertAfter(3);
+sll.insertAfter(4);
+sll.insertAfter(5);
+sll.insertAfter(6);
+sll.insertAfter(7);
+sll.insertAfter(8);
+sll.insertAfter(9);
+console.log(JSON.stringify(sll, null, 2));
